3a6f6089f5d10c3be7ce10710037fac07f516512
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2018-12-27 13:06:19 +0100

    ConnectionManager

--- src/infrastructure/ConnectionManager.java
+++ src/infrastructure/ConnectionManager.java
@@ -6,8 +6,9 @@ import java.util.Properties;
 
 public class ConnectionManager {
     private static Connection connection;
+    private static Statement statement;
 
-    public static void connect(String username, String password){
+    public static void connect(String username, String password) {
         System.out.println("Connecting to database...");
         connection = null;
         Properties properties = new Properties();
@@ -15,15 +16,26 @@ public class ConnectionManager {
         properties.put("password", password);
         try {
             connection = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/" + "xe", properties);
-            Statement statement = connection.createStatement();
+            statement = connection.createStatement();
             ResultSet rs = statement.executeQuery("select owner, table_name from all_tables where owner = '" + username + "'");
             System.out.println(username + "'s tables in schema:");
-            while(rs.next()){
+            while (rs.next()) {
                 System.out.println(rs.getString(2));
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
+    public static ResultSet getStatementResultSet(String SqlStatement) {
+        try {
+            return (ResultSet) statement.executeQuery(SqlStatement);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Invalid SQL statement");
+        }
+        return null;
+    }
+
 
 }
